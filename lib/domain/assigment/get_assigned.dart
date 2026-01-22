0b655f3a18cea9df3aa3ecbe4c3d91adde640ae8
PedroArt <pedroarteta@hotmail.es>
2026-01-22 16:44:15 -0500

    Cambio de Clase

--- lib/domain/assigment/get_assigned.dart
+++ lib/domain/assigment/get_assigned.dart
@@ -2,6 +2,6 @@ import 'package:app_asistencias/models/assigment_model.dart';
 
 class GetAssigned {
   static Future<List<AssigmentModel>> fetchAssignment() async {
-    return [];
+    return [] ;
   }
 }
